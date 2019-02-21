0e6b01c0680fb5e43a62fc2ef7c5d96851b2450f
lst97 (Nelson) <laisiotou1997@gmail.com>
2019-02-21 11:04:28 +1100

    Add files via upload

---
+++ Exercise 1 My First Program/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Exercise_1_My_First_Program_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxGName = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelGName = new System.Windows.Forms.Label();
+            this.labelFName = new System.Windows.Forms.Label();
+            this.labelMShow = new System.Windows.Forms.Label();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxGName
+            // 
+            this.textBoxGName.Location = new System.Drawing.Point(156, 73);
+            this.textBoxGName.Name = "textBoxGName";
+            this.textBoxGName.Size = new System.Drawing.Size(136, 20);
+            this.textBoxGName.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.Red;
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(374, 55);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "HELLO WORLD";
+            // 
+            // labelGName
+            // 
+            this.labelGName.AutoSize = true;
+            this.labelGName.Location = new System.Drawing.Point(80, 76);
+            this.labelGName.Name = "labelGName";
+            this.labelGName.Size = new System.Drawing.Size(69, 13);
+            this.labelGName.TabIndex = 2;
+            this.labelGName.Text = "Given Name:";
+            // 
+            // labelFName
+            // 
+            this.labelFName.AutoSize = true;
+            this.labelFName.Location = new System.Drawing.Point(80, 122);
+            this.labelFName.Name = "labelFName";
+            this.labelFName.Size = new System.Drawing.Size(70, 13);
+            this.labelFName.TabIndex = 3;
+            this.labelFName.Text = "Family Name:";
+            // 
+            // labelMShow
+            // 
+            this.labelMShow.AutoSize = true;
+            this.labelMShow.Location = new System.Drawing.Point(153, 163);
+            this.labelMShow.Name = "labelMShow";
+            this.labelMShow.Size = new System.Drawing.Size(77, 13);
+            this.labelMShow.TabIndex = 4;
+            this.labelMShow.Text = "MessageShow";
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(156, 119);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(136, 20);
+            this.textBoxFName.TabIndex = 5;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(83, 198);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnter.TabIndex = 6;
+            this.buttonEnter.Text = "ENTER!";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Location = new System.Drawing.Point(217, 198);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbout.TabIndex = 7;
+            this.buttonAbout.Text = "ABOUT";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 242);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.labelMShow);
+            this.Controls.Add(this.labelFName);
+            this.Controls.Add(this.labelGName);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBoxGName);
+            this.Name = "Form1";
+            this.Text = "Exercise 1 My First Program_S10000399";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxGName;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelGName;
+        private System.Windows.Forms.Label labelFName;
+        private System.Windows.Forms.Label labelMShow;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonAbout;
+    }
+}
+
---
+++ Exercise 1 My First Program/Form1.cs
@@ -0,0 +1,64 @@
+﻿/*
+;|**********************************************************************;
+;* Project           : Basic_CSharp
+;*
+;* Program name      : Basic.cs
+;*
+;* Author            : SIO TOU LAI (laisiotou1997@gmail.com)
+;*
+;* Date created      : 21/02/2019
+;*
+;* Version           : Internal
+;*
+;* Copyright         : GNU GENERAL PUBLIC LICENSE Version 3
+;*
+;* Purpose           : HelloWorld_BHI
+;*
+;* Revision History  :
+;*
+;* Date        Author      Ref    Revision (Date in DDMMYYYY format)
+;* 21022019    lst97       1      First release
+;*
+;* Known Issue       :
+;*
+;|**********************************************************************;
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_1_My_First_Program_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Project\t\t: Basic_CSharp\n\nProgram\t\t: Basic.cs\n\nAuthor\t\t: SIO TOU LAI (laisiotou1997@gmail.com)\n\nDate created\t: 21/02/2019\n\nVersion\t\t: Internal\n\nCopyright\t\t: GNU GENERAL PUBLIC LICENSE Version 3\n\nPurpose\t\t: HelloWorld_BHI\n\nRevision History  :\nDate\tAuthor\tRef\tRevision (Date in DDMMYYYY format)\n21022019\tlst97\t1\tFirst release", "ABOUT");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxGName.Text == "" || textBoxFName.Text == "" )
+            {
+                MessageBox.Show("Invaild Input!", "Error");
+            }
+            else{
+                MessageBox.Show("Welcome: \"" + textBoxGName.Text + " " + textBoxFName.Text + "\"", "Message");
+                labelMShow.Text = (textBoxGName.Text + " " + textBoxFName.Text);
+            }
+            
+        }
+    }
+}
